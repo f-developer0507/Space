e2e083844b419e154141621c149b5f73c6641770
f-developer0507 <stormpubg0507@gmail.com>
2024-01-18 20:00:01 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  const preloader = document.querySelector(".preloader-wrapper");
+  preloader.classList.add("preloader-hidden");
+  setInterval(function () {
+    preloader.classList.add("preloader-hidden");
+  }, 1000);
+};
+
+const burgerBtn = document.querySelector(".header__burger");
+const headerNav = document.querySelector(".header__nav");
+
+burgerBtn.addEventListener("click", function () {
+  burgerBtn.classList.toggle("active");
+  headerNav.classList.toggle("header__nav--mobile");
+});
